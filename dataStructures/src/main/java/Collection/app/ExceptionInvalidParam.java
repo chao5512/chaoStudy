4b2d43961e91397d71e58c4263bfaecb2cfe1bc3
wangchao <543164029@qq.com>
2018-08-09 10:51:17 +0800

    参数非法异常

---
+++ dataStructures/src/main/java/Collection/app/ExceptionInvalidParam.java
@@ -0,0 +1,10 @@
+package Collection.app;
+
+/**
+ * Created by wangchao on 2018/8/9.
+ */
+public class ExceptionInvalidParam extends RuntimeException{
+    public ExceptionInvalidParam(String msg) {
+        super(msg);
+    }
+}
