187681329415c9be888df27471f0d743bdd188e2
wangchao <543164029@qq.com>
2019-12-04 01:44:55 +0800

    自顶向下的merge

--- algorithm/src/main/java/sort/MergeSort.java
+++ algorithm/src/main/java/sort/MergeSort.java
@@ -1,10 +1,16 @@
 package sort;
 
+import utils.ArrayUtils;
+
 /**
- * 自顶向下的归并排序.
+ * 自顶向下的归并排序
  * Created by wangchao on 2018/7/11.
  */
 public class MergeSort extends Sort {
+
+    public MergeSort(String name) {
+        super(name);
+    }
 
     @Override
     public void sort(Comparable[] a) {
@@ -14,10 +20,11 @@ public class MergeSort extends Sort {
         sort(a, aux,lo, hi);
     }
 
-    private void sort(Comparable[] a,Comparable[]aux, int lo, int hi) {
+    protected void sort(Comparable[] a,Comparable[]aux, int lo, int hi) {
         if (lo >= hi) {
             return;
         }
+        //不使用(lo+hi)/2这种形式找中点的原因之一是下面这样不会发生（lo+hi）溢出的错误
         int mid = lo + ((hi - lo)>>>1);
         sort(a,aux ,lo, mid);
         sort(a,aux ,mid + 1, hi);
@@ -25,30 +32,37 @@ public class MergeSort extends Sort {
     }
 
     //todo
-    private void merge(Comparable[] a,Comparable[] aux, int lo, int mid, int hi) {
+    protected void merge(Comparable[] a,Comparable[] aux, int lo, int mid, int hi) {
         assert isSorted(a, lo, mid);
         assert isSorted(a, mid + 1, hi);
-        //如果已经有序了，直接返回原数组
-        if(lessThan(a[mid],a[mid+1]))return;
+
+
+        //如果已经有序了，直接返回原数组,递归基
+        if(!lessThan(a[mid+1],a[mid]))return;
         //
         for (int i = lo; i <= hi; i++) {
             aux[i] = a[i];
         }
+        //分成p,q前后两个数组。
         for(int i = lo, p = lo, q = mid+1; i <= hi; i++) {
-            if (q > hi || p < mid+1 && lessThan(aux[p],aux[q]))
+            if (q > hi || p < mid+1 && !lessThan(aux[q],aux[p]))
+                //p归队的情况
+                //1. q > hi q数组已经为空
+                //2. 或者在p不为空（p<mid+1）的前提下，p的首元素小于等于(等于这个条件是为了稳定性)q的寿元素
                 a[i] = aux[p++];
             else
+                //其他情况全是q
                 a[i] = aux[q++];
         }
     }
 
     public static void main(String[] args) {
         //, "zhang", "san", "li", "si"
-        String[] a = {"wang", "er", "ma", "zi", "zhang", "san", "li", "si"};
+        //String[] a = {"wang", "er", "ma", "zi", "zhang", "san", "li", "si"};
 //        String[] a = {"zhang", "san", "li", "si"};
-        Sort sort = new MergeSort();
-        sort.sort(a);
-        assert isSorted(a);
-        show(a);
+        Integer[] a = ArrayUtils.makeArray(200000, 3000);
+        Sort sort = new MergeSort("归并排序");
+        sort.testSort(a);
+
     }
 }
